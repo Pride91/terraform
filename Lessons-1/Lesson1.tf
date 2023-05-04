8fefa06d9781176542f3dc9edc55938091d8ccc4
Viktor Khvorostyna <jimbooo991@gmail.com>
2023-05-04 15:59:17 +0300

    test

--- Lessons-1/Lesson1.tf
+++ Lessons-1/Lesson1.tf
@@ -9,3 +9,8 @@ resource "aws_instance" "my_ubuntu" {
   ami = "ami-0c6c29c5125214c77"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "my_ubuntu" {
+  ami = "ami-0c6c29c5125214c77"
+  instance_type = "t2.micro"
+}
