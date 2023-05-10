0db89d75924424c135efe7dee7995eb473d08c6b
Viktor Khvorostyna <jimbooo991@gmail.com>
2023-05-10 14:10:10 +0300

    saved

--- Lessons-1/Lesson1.tf
+++ Lessons-1/Lesson1.tf
@@ -1,11 +1,16 @@
 provider "aws" {
   access_key = "AKIAYJMU63ECV6DNX3UX"
   secret_key = "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"
-  region = "us-east-1"
+  region     = "us-east-1"
 
 }
 
 resource "aws_instance" "my_ubuntu" {
-  ami = "ami-07625524674f7c390"
+  ami           = "ami-07625524674f7c390"
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "my_ubuntu" {
+  ami           = "ami-07625524674f7c390"
   instance_type = "t2.micro"
 }
